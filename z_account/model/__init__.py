5b457964d778e6cbb63bba7223ade210452a5ff3
an-pc <lehoangan1988@gmail.com>
2015-10-05 10:21:08 +0700

    domain for account move

--- z_account/model/__init__.py
+++ z_account/model/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import model,report,wizard
+import account_move
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

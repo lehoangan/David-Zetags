5338cf0bf77d1ca7c5804056f9cc87e00e523fb9
Jack <lehoangan1988@gmail.com>
2016-05-08 15:43:24 +0700

    2 step login odoo

--- auth_2step/models/login.py
+++ auth_2step/models/login.py
@@ -5,7 +5,6 @@ def login(db, login, password):
     pool = pooler.get_pool(db)
     user_obj = pool.get('res.users')
     res = user_obj.login(db, login, password)
-    print '==============', res
     return res
 
 security.login=login
